33e337394896c0bbe13a02ca42247fd5c4874cda
root <root@ip-172-31-37-216.ec2.internal>
2017-11-18 19:55:24 +0000

    minor changes

--- alexnet_bn_train.py
+++ alexnet_bn_train.py
@@ -5,7 +5,7 @@ from tensorflow.contrib.layers.python.layers import batch_norm
 from DataLoader import *
 
 # Dataset Parameters
-batch_size = 100
+batch_size = 50
 load_size = 256
 fine_size = 224
 c = 3
@@ -30,28 +30,28 @@ def batch_norm_layer(x, train_phase, scope_bn):
     
 def alexnet(x, keep_dropout, train_phase):
     weights = {
-        # 'wc1': tf.Variable(tf.random_normal([11, 11, 3, 96], stddev=np.sqrt(2./(11*11*3)))),
-        # 'wc2': tf.Variable(tf.random_normal([5, 5, 96, 256], stddev=np.sqrt(2./(5*5*96)))),
-        # 'wc3': tf.Variable(tf.random_normal([3, 3, 256, 384], stddev=np.sqrt(2./(3*3*256)))),
-        # 'wc4': tf.Variable(tf.random_normal([3, 3, 384, 256], stddev=np.sqrt(2./(3*3*384)))),
-        # 'wc5': tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=np.sqrt(2./(3*3*256)))),
+        'wc1': tf.Variable(tf.random_normal([7, 7, 3, 96], stddev=np.sqrt(2./(11*11*3)))),
+        'wc2': tf.Variable(tf.random_normal([5, 5, 96, 256], stddev=np.sqrt(2./(5*5*96)))),
+        'wc3': tf.Variable(tf.random_normal([3, 3, 256, 384], stddev=np.sqrt(2./(3*3*256)))),
+        'wc4': tf.Variable(tf.random_normal([3, 3, 384, 256], stddev=np.sqrt(2./(3*3*384)))),
+        'wc5': tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=np.sqrt(2./(3*3*256)))),
 		#
-        # 'wf6': tf.Variable(tf.random_normal([7*7*256, 4096], stddev=np.sqrt(2./(7*7*256)))),
-        # 'wf7': tf.Variable(tf.random_normal([4096, 4096], stddev=np.sqrt(2./4096))),
-        # 'wo': tf.Variable(tf.random_normal([4096, 100], stddev=np.sqrt(2./4096)))
+        'wf6': tf.Variable(tf.random_normal([7*7*256, 4096], stddev=np.sqrt(2./(7*7*256)))),
+        'wf7': tf.Variable(tf.random_normal([4096, 4096], stddev=np.sqrt(2./4096))),
+        'wo': tf.Variable(tf.random_normal([4096, 100], stddev=np.sqrt(2./4096)))
 
         # W = tf.get_variable("W", shape=[784, 256],
         #                     initializer=tf.contrib.layers.xavier_initializer())
 
-        'wc1': tf.get_variable('wc1', shape=[11, 11, 3, 96], initializer=tf.contrib.layers.xavier_initializer()),
-        'wc2': tf.get_variable('wc2', shape=[5, 5, 96, 256], initializer=tf.contrib.layers.xavier_initializer()),
-        'wc3': tf.get_variable('wc3', shape=[3, 3, 256, 384], initializer=tf.contrib.layers.xavier_initializer()),
-        'wc4': tf.get_variable('wc4', shape=[3, 3, 384, 256], initializer=tf.contrib.layers.xavier_initializer()),
-        'wc5': tf.get_variable('wc5', shape=[3, 3, 256, 256], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wc1': tf.get_variable('wc1', shape=[7,7, 3, 96], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wc2': tf.get_variable('wc2', shape=[5, 5, 96, 256], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wc3': tf.get_variable('wc3', shape=[3, 3, 256, 384], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wc4': tf.get_variable('wc4', shape=[3, 3, 384, 256], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wc5': tf.get_variable('wc5', shape=[3, 3, 256, 256], initializer=tf.contrib.layers.xavier_initializer()),
 
-        'wf6': tf.get_variable('wf6', shape=[7 * 7 * 256, 4096], initializer=tf.contrib.layers.xavier_initializer()),
-        'wf7': tf.get_variable('wf7', shape=[4096, 4096],initializer=tf.contrib.layers.xavier_initializer()),
-        'wo': tf.get_variable('wo',shape=[4096, 100], initializer=tf.contrib.layers.xavier_initializer())
+        #'wf6': tf.get_variable('wf6', shape=[7 * 7 * 256, 4096], initializer=tf.contrib.layers.xavier_initializer()),
+        #'wf7': tf.get_variable('wf7', shape=[4096, 4096],initializer=tf.contrib.layers.xavier_initializer()),
+        #'wo': tf.get_variable('wo',shape=[4096, 100], initializer=tf.contrib.layers.xavier_initializer())
     }
 
     biases = {                            
@@ -59,13 +59,13 @@ def alexnet(x, keep_dropout, train_phase):
     }
 
     # Conv + ReLU + Pool, 224->55->27
-    conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 4, 4, 1], padding='SAME')
+    conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 2, 2, 1], padding='SAME')
     conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
     conv1 = tf.nn.relu(conv1)
     pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Conv + ReLU  + Pool, 27-> 13
-    conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 1, 1, 1], padding='SAME')
+    conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 2, 2, 1], padding='SAME')
     conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
     conv2 = tf.nn.relu(conv2)
     pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
@@ -106,7 +106,7 @@ def alexnet(x, keep_dropout, train_phase):
 
 # Construct dataloader
 opt_data_train = {
-    'data_h5': 'miniplaces_256_train.h5',
+    #'data_h5': 'miniplaces_256_train.h5',
     'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
     'data_list': '../../data/train.txt', # MODIFY PATH ACCORDINGLY
     'load_size': load_size,
@@ -115,7 +115,7 @@ opt_data_train = {
     'randomize': True
     }
 opt_data_val = {
-    'data_h5': 'miniplaces_256_val.h5',
+    #'data_h5': 'miniplaces_256_val.h5',
     'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
     'data_list': '../../data/val.txt',   # MODIFY PATH ACCORDINGLY
     'load_size': load_size,
@@ -124,10 +124,10 @@ opt_data_val = {
     'randomize': False
     }
 
-#loader_train = DataLoaderDisk(**opt_data_train)
-#loader_val = DataLoaderDisk(**opt_data_val)
-loader_train = DataLoaderH5(**opt_data_train)
-loader_val = DataLoaderH5(**opt_data_val)
+loader_train = DataLoaderDisk(**opt_data_train)
+loader_val = DataLoaderDisk(**opt_data_val)
+#loader_train = DataLoaderH5(**opt_data_train)
+#loader_val = DataLoaderH5(**opt_data_val)
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, fine_size, fine_size, c])
@@ -230,3 +230,4 @@ with tf.Session() as sess:
     string = 'Evaluation Finished! Accuracy Top1 = ' + "{:.4f}".format(acc1_total) + ", Top5 = " + "{:.4f}".format(acc5_total)
     f.write(string)
     print(string)
+
